703b64a699b35099079c9f1e9c9ae69099cecee7
ramos <rdiego26@gmail.com>
2017-03-04 15:41:54 -0300

    ADD advertisement category model .
    #4

---
+++ src/model/advertisementCategory.js
@@ -0,0 +1,22 @@
+const path = require('path'),
+    sequelize = require(path.resolve('src/util/sequelize-connection')),
+    Sequelize = require('sequelize');
+
+const advertisementCategory = sequelize.define('advertisement_category', {
+
+  id: {
+    type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true
+  },
+  name: {
+    type: Sequelize.STRING(25), allowNull: false, unique: true
+  }
+},
+    {
+      freezeTableName: true,
+      createdAt: false,
+      updatedAt: false,
+      tableName: 'advertisement_category'
+    }
+);
+
+module.exports = advertisementCategory;
