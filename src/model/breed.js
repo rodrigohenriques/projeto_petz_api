6df1d9e94f5eb7ed52aa150f50d67106559c6072
ramos <rdiego26@gmail.com>
2017-03-04 14:29:00 -0300

    ADD breed model . #4

---
+++ src/model/breed.js
@@ -0,0 +1,22 @@
+const path = require('path'),
+    sequelize = require(path.resolve('src/util/sequelize-connection')),
+    Sequelize = require('sequelize');
+
+const breed = sequelize.define('breed', {
+
+  id: {
+    type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true
+  },
+  name: {
+    type: Sequelize.STRING(80), allowNull: false, unique: true
+  }
+},
+    {
+      freezeTableName: true,
+      createdAt: false,
+      updatedAt: false,
+      tableName: 'breed'
+    }
+);
+
+module.exports = breed;
