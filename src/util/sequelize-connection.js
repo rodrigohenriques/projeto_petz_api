7e00a564cc1cde343a77fd5cddfefaf7ed794f3f
ramos <rdiego26@gmail.com>
2017-03-06 20:46:52 -0300

    USE let and increase min pool .

--- src/util/sequelize-connection.js
+++ src/util/sequelize-connection.js
@@ -1,12 +1,12 @@
-var Sequelize = require('sequelize'),
+let Sequelize = require('sequelize'),
     constants = require('./constants');
-var sequelize = new Sequelize(constants.db.database, constants.db.username, constants.db.password, {
+let sequelize = new Sequelize(constants.db.database, constants.db.username, constants.db.password, {
   host: constants.db.host,
   dialect: 'postgres',
   logging: false,
   pool: {
     max: 20,
-    min: 5,
+    min: 8,
     idle: 10000
   }
 });
