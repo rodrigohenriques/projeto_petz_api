45489288cbc8887334e2ffe066eb4e8b14dc9aec
ramos <rdiego26@gmail.com>
2017-03-06 22:45:50 -0300

    ADD update advertisementPhoto update .
    #4

--- src/dao/advertisementPhoto.js
+++ src/dao/advertisementPhoto.js
@@ -1,10 +1,19 @@
 const path = require('path'),
+	R = require('ramda'),
 	advertisementPhotoModel = require(path.resolve('src/model/advertisementPhoto'));
 
 const dao = {
 
   create: function(advertisementPhoto) {
     return advertisementPhotoModel.create(advertisementPhoto);
+  },
+
+  update: function(newObject, filter) {
+
+    newObject = R.omit(['advertisement_id'], newObject);
+
+    return advertisementPhotoModel.update(newObject, { returning: true, where: filter });
+
   }
 
 };
